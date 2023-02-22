c2650287ac642e1782461a8dd7c7d20dfec84f11
Juan Diego Lopez <juandlopez@msn.com>
2023-02-22 17:30:41 -0500

    feat(setup): initial commit adding react router, redux toolkit and material ui

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.scss';
+
+const App: React.FC = () => {
+  return <h1 className={styles.title}>The Good Pizza Club</h1> 
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss';
+import CssBaseline from '@mui/material/CssBaseline';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { Provider } from 'react-redux';
+import store from './app/core/store/store';
+import { ThemeProvider } from '@mui/material';
+import theme from './app/core/theme/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
