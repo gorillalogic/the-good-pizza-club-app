b7d0fa187a1ed0b22c0213f2d4278dff08272bab
Juan Diego Lopez <juandlopez@msn.com>
2023-02-22 18:16:47 -0500

    feat(setup): add not found component

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './app/App'
+import NotFound from './app/pages/NotFound/NotFound'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <NotFound />,
   },
 ])
 
