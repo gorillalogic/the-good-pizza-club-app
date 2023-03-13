f699ab86423a8ae4ba78566df8026b38357c4b90
Juan Diego Lopez <juan.lopez@gorillalogic.com>
2023-03-13 16:14:56 -0500

    feat: add axios interceptors to redirect to index page if any request get 403 error

--- src/core/services/axios.ts
+++ src/core/services/axios.ts
@@ -1,9 +1,27 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { LOCALSTORAGE_KEYS } from '../../shared/constants/global.constants';
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:7777/api',
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    const { response } = error as AxiosError;
+
+    if (response?.status === 403) {
+      localStorage.removeItem(LOCALSTORAGE_KEYS.user);
+      window.location.replace('/');
+    }
+
+    return Promise.reject(error);
+  }
+);
